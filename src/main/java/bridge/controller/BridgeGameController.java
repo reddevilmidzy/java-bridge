43d288a1e411b79c557fdc857e3365ea3fc8ada3
reddevilmidzy <midzy0228@gmail.com>
2023-10-19 01:36:00 +0900

    feat: 게임 재시작/종료 여부 파악후 종료/재시작

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -2,6 +2,7 @@ package bridge.controller;
 
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.model.Command;
 import bridge.model.Status;
 import bridge.service.BridgeGame;
 import bridge.view.InputView;
@@ -11,7 +12,7 @@ import java.util.List;
 public class BridgeGameController {
 
     // TODO: 이 부분 DI 적용
-    private BridgeGame bridgeGame = new BridgeGame();
+    private final BridgeGame bridgeGame = new BridgeGame();
     private final InputView inputView;
 
     public BridgeGameController(InputView inputView) {
@@ -21,17 +22,27 @@ public class BridgeGameController {
     public void run() {
         String bridgeSize = inputView.readBridgeSize();
         // TODO: 검증 로직
+        // TODO: 동강동강열매 먹은 듯이 컷팅
         int size = Integer.parseInt(bridgeSize);
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         List<String> bridge = bridgeMaker.makeBridge(Integer.parseInt(bridgeSize));
 
         System.out.println("bridge = " + bridge);
+        start(size, bridge);
+    }
 
+    private void start(Integer size, List<String> bridge) {
         for (int i = 0; i < size; i++) {
             String moving = inputView.readMoving();
             if (bridgeGame.move(Status.valueOf(bridge.get(i)), Status.valueOf(moving))) {
                 System.out.println("상태 출력");
                 continue;
+            }
+            String gameCommand = inputView.readGameCommand();
+            if (Command.valueOf(gameCommand).equals(Command.R)) {
+                bridgeGame.retry();
+                start(size, bridge);
+                return;
             }
             break;
         }
