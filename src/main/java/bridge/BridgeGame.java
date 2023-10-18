86c0618accab7aa3c041ac3a75a2039f26dfd12a
reddevilmidzy <midzy0228@gmail.com>
2023-10-19 00:18:17 +0900

    feat: 다리 길이 입력

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,22 @@
 package bridge;
 
+import bridge.view.InputView;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    InputView inputView;
+
+    public BridgeGame(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void run() {
+        String bridgeSize = inputView.readBridgeSize();
+
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
